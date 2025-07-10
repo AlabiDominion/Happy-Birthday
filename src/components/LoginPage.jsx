a46c59c01c252bbf68cb1f9a79e934b15f95c4ed
AlabiDominion <alabidominion@gmail.com>
2025-07-10 20:15:36 +0100

    Completed the login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,56 +1,89 @@
 import { useState } from "react";
+import Loader from "./Loader";
 
 const LoginPage = ({ onLogin }) => {
   const [inputCode, setInputCode] = useState("");
   const [inputName, setInputName] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = () => {
-    if (inputCode === "2025" && inputName === "Pookie") {
-      onLogin();
-    } else {
-      setError("Invalid code. Try again!");
-    }
+    setError("");
+    setLoading(true);
+
+    setTimeout(() => {
+      if (inputCode === "2025" && inputName === "Pookie") {
+        onLogin();
+      } else {
+        setError("Invalid code. Try again!");
+      }
+      setLoading(false);
+    }, 3000);
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center  bg-[#F5F0EA] relative overflow-hidden">
+    <div className="LoginLoader min-h-screen flex flex-col items-center bg-[#F5F0EA] relative overflow-hidden">
+     
       <img className="absolute top-0 left-0 z-[100]" src="./Ribbon.png" alt="decoration1" />
-      <img className="absolute z-100 top-0 right-0" src="./ribbon2.png" alt="decoration2" />
-      <img className="absolute z-100 bottom-0 right-0" src="./ribbon3.png" alt="decoration3" />
+      <img className="absolute z-[100] top-0 right-0" src="./ribbon2.png" alt="decoration2" />
+      <img className="absolute z-[100] bottom-0 right-0" src="./ribbon3.png" alt="decoration3" />
 
+      
       <img className="mt-[4.8rem] mb-2" src="./logo.png" alt="logo" />
       <img src="./HAPPY.png" alt="Happy" />
-      <img className="absolute top-[114px]  z-[99]" src="./HappyBirthdayText.png" alt="Birthday" />
-      <p className="mt-[6.7rem] font-quicksand font-[400]">Just wanna make sure it's you mama💝💖</p>
+      <img className="absolute top-[114px] z-[99]" src="./HappyBirthdayText.png" alt="Birthday" />
 
-      <form action="Submit" className="flex flex-col mt-[3rem] ml-auto mr-auto w-[80%] ">
-        <label className="font-quicksand mb-1" htmlFor="Name">My name for you:</label>
+      <p className="mt-[6.7rem] font-quicksand font-[400]">
+        Just wanna make sure it's you mama💝💖
+      </p>
+
+      
+      <form
+        className="flex flex-col mt-[3rem] ml-auto mr-auto w-[80%]"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <label className="font-quicksand mb-1" htmlFor="Name">
+          My name for you:
+        </label>
         <input
           type="text"
           id="Name"
           value={inputName}
           onChange={(e) => setInputName(e.target.value)}
-          className="bg-[#ECE4DB] rounded-[16px] h-[50px] p-2 text-[]"
+          className="bg-[#ECE4DB] rounded-[16px] h-[50px] p-2"
+          disabled={loading}
         />
-        <label className="mt-[1rem] font-quicksand mb-1" htmlFor="Name">The password:</label>
+
+        <label className="mt-[1rem] font-quicksand mb-1" htmlFor="password">
+          The password:
+        </label>
         <input
           type="password"
           id="password"
           value={inputCode}
           onChange={(e) => setInputCode(e.target.value)}
-          className="bg-[#ECE4DB] rounded-[16px] h-[50px]"
+          className="bg-[#ECE4DB] rounded-[16px] h-[50px] p-2"
+          disabled={loading}
         />
       </form>
+
+      
       <button
         onClick={handleLogin}
-        className="mt-[4rem] bg-[#871058] rounded-[24px] h-[50px] w-[50%] font-quicksand text-[#D9CFC4] text-[1.1rem]"
+        className="mt-[4rem] bg-[#871058] rounded-[24px] h-[50px] w-[50%] font-quicksand text-[#D9CFC4] text-[1.1rem] z-[101]"
+        disabled={loading}
       >
         Unlock Secret →
       </button>
+
       {error && <p className="text-red-500 mt-2">{error}</p>}
 
-
+   
+      {loading && (
+        <div className="fixed inset-0 z-[999] bg-black/50 flex items-center justify-center">
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
